e4183b386b2e60234511021074d93d7e389b13ac
Willyissa2 <139506965+Willyissa2@users.noreply.github.com>
2023-08-14 15:21:08 -0500

    Delete coinflip.py

--- coinflip.py
+++
@@ -1,8 +0,0 @@
-weight = int(input("Enter your weight in pounds: "))
-
-height = int(input("Enter your height in inches: "))
-
-BMI = (weight * 703) / (height * height)
-
-print(BMI)
-#BMI = (weight in pounds x 703) / (height in inches x height in inches)
